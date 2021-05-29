040ece07d36ddb3a1108cf647becae872bdbf1c4
Victor Silva <victor.psilva@viavarejo.com.br>
2021-05-29 14:20:27 -0300

    feat: adding blank login component

--- apps/viahacka-team4-app/src/app/app.module.ts
+++ apps/viahacka-team4-app/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
