a35003dc1b3332020e74236baf715da310e5d373
Victor Silva <victor.psilva@viavarejo.com.br>
2021-05-29 14:26:07 -0300

    feat: adding routing module

--- apps/viahacka-team4-app/src/app/app.module.ts
+++ apps/viahacka-team4-app/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+
 @NgModule({
   declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
