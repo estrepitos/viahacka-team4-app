65498dca66d6b7c32c74d852eea36075987826ca
Victor Silva <victor.psilva@viavarejo.com.br>
2021-05-29 14:30:57 -0300

    feature: creating home component

--- apps/viahacka-team4-app/src/app/app.module.ts
+++ apps/viahacka-team4-app/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
